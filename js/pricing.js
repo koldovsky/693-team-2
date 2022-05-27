9ac9141d3f75ff65ed2ec19516505c3561cf3531
Kateryna Danylo <aleksandrova414@gmail.com>
2022-05-27 16:06:38 +0300

    add new class with js in my pricing section

--- js/pricing.js
+++ js/pricing.js
@@ -1,4 +1,15 @@
 (function () {
-        let pricingContainer = document.querySelectorAll(".container_pricing_li");
-        console.log(pricingContainer);
+        const pricingList = document.querySelector(".pricing_list");
+        console.log(pricingList);
+
+
+        window.addEventListener('scroll', function () {
+                let scrollPos = window.scrollY;
+                console.log(scrollPos);
+                if (scrollPos > 9100 && scrollPos<9800 ) {
+                        pricingList.classList.add('visible');
+                }else{
+                        pricingList.classList.remove('visible');
+                }
+        })
 })();
