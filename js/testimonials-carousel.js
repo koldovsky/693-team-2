282d5bcc96d0c24c3ae7c8d6cee0db47f2a1c163
vitkarik <vitkarik27@gmail.com>
2022-05-27 10:18:36 +0000

    Work js

--- js/testimonials-carousel.js
+++ js/testimonials-carousel.js
@@ -1,23 +1,23 @@
-(function () {
+(function(){
     const slides = [
-      '<div class= "slide"><img src = "img/testimonials.linda.jpg" alt= "Linda"</div>',
-      '<div class= "slide"><img src = "img/testimonials.devid.jpg" alt= "Devid"</div>',
-      '<div class= "slide"><img src = "img/testimonials.kyli.jpg" alt= "Kyli"</div>',
-      '<div class= "slide"><img src = "img/testimonials.lucas.jpg" alt= "Lucas"</div>',
+      '<div class= "slide"><img src = "img/testimonials/linda.jpg" alt= "Linda"</div>',
+      '<div class= "slide"><img src = "img/testimonials/devid.jpg" alt= "Devid"</div>',
+      '<div class= "slide"><img src = "img/testimonials/kyli.jpg" alt= "Kyli"</div>',
+      '<div class= "slide"><img src = "img/testimonials/lucas.jpg" alt= "Lucas"</div>',
     ];
   
     let currentSlide = 0;
   
     function renderCarousel() {
-      const carouselContainer = document.querySelector(".testimonials-carousel");
-      carouselContainer.innerHTML = slides[currentSlide];
+      const slidelContainer = document.querySelector(".testimonials-carousel .slide");
+      slidelContainer.innerHTML = slides[currentSlide];
     }
   
     function nextSlide() {
-      currentSlide = currentSlide + 1 >= slides.length ? 0 : urrentSlide + 1;
+      currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
       renderCarousel();
     }
   
-    setInterval(nextSlide, 3000);
+    setInterval(nextSlide, 1000);
     renderCarousel();
-  });
+  }) ();
