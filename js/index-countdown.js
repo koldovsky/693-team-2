68f44f34e4c67208d18d86e4ba0be427c158bf4b
PawloBryk <pawlobryk95@gmail.com>
2022-05-27 08:45:09 +0000

    finished coundown js part

--- js/index-countdown.js
+++ js/index-countdown.js
@@ -1,3 +1,23 @@
-(function(){
-    
-})
+
+const countdown = () => {
+    const countDay = new Date("June 27, 2022, 00:00:00").getTime();
+    const currentTime = new Date().getTime();
+    const timeToOpen = countDay - currentTime;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const timeDay = Math.floor(timeToOpen / day);
+    const timeHour = Math.floor((timeToOpen % day)/hour);
+    const timeMinute = Math.floor((timeToOpen % hour)/minute);
+    const timeSecond = Math.floor((timeToOpen % minute)/second);
+
+    document.querySelector('.countdown-day').innerText = timeDay;
+    document.querySelector('.countdown-hour').innerText = timeHour;
+    document.querySelector('.countdown-minute').innerText = timeMinute;
+    document.querySelector('.countdown-second').innerText = timeSecond;
+}
+
+setInterval(countdown, 1000);
