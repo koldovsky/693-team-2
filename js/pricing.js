fe189c61c0b4f70ae79fd58be4bbc4ba2e2627ef
Kateryna Danylo <aleksandrova414@gmail.com>
2022-05-27 17:14:21 +0300

    remove mistake from my code in pricing.js

--- js/pricing.js
+++ js/pricing.js
@@ -1,27 +1,24 @@
 (function () {
         const pricingList = document.querySelector(".pricing_list");
-        const firstPricingBtn = document.getElementById('first_pricing_btn');
         const pricingBtn = document.querySelectorAll(".pricing_btn");
 
 
-        firstPricingBtn.addEventListener('click', function(){
-               // console.log("clicked");
-        });
-
-
-        window.addEventListener('scroll', function () {
-                let scrollPos = window.scrollY;
-                console.log(scrollPos);
-                if (scrollPos > 9500 && scrollPos<9900 ) {
-                        pricingList.classList.add('visible');
-                }else{
-                        pricingList.classList.remove('visible');
-                }
-        });
+        changeBackground(pricingList);
 
         for (let btns of pricingBtn){
                 btns.addEventListener('click', function(){
-                        console.log("clicked");
+                        console.log("cliked");
                 });
         };
 })();
+
+function changeBackground(pricingList) {
+        window.addEventListener('scroll', function () {
+                let scrollPos = window.scrollY;
+                if (scrollPos > 9000 && scrollPos < 9900) {
+                        pricingList.classList.add('visible');
+                } else {
+                        pricingList.classList.remove('visible');
+                }
+        });
+}
