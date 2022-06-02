2f2a18fbcafa0b5f6699c004230f7f0b08a32cf5
Kateryna Danylo <aleksandrova414@gmail.com>
2022-06-02 18:17:15 +0300

    remove mistake with my js

--- js/pricing.js
+++ js/pricing.js
@@ -1,32 +1,16 @@
 (function () {
         const pricingList = document.querySelector(".pricing_list");
         const pricingBtn = document.querySelectorAll(".pricing_btn");
-        // const containerFromPosition = document.querySelector(".pricing_list");
-        // const backgroundPosition = containerFromPosition.getBoundingClientRect();
-
-        // changeBackground(pricingList);
-
-        // function changeBackground(pricingList) {
-        //         window.addEventListener('scroll', function () {
-        //                 if ((backgroundPosition.top <= 200) && (backgroundPosition.top > (-600))) {
-        //                         pricingList.classList.add('visible');
-        //                 } else {
-        //                         pricingList.classList.remove('visible');
-        //                 }
-
-
-        //         });
-        // }
-
+        const modalCart = document.querySelector('.modal')
         for (let btns of pricingBtn) {
-                btns.addEventListener('click', function (event) {
-                        console.log(event);
+                btns.addEventListener('click', function () {
+                          modalCart.classList.add('.visible');
                 });
         };
 
 
         function renderProducts(products){
-                for (let product in products)
+                for (let product of products)
                 pricingList.innerHTML += `<li class="pricing_li">
                 <div class="container_pricing_li">
                     <div class="pricing_icon">
@@ -48,37 +32,37 @@
                 </div>
             </li>`;
         };
-        const productJSON =`[
-            {   
-                "id": "1",
-                "name": "ESTANDAR",
-                "countInPack": 10,
-                "price": 12, 
-                "imgUrl": "img/icons/icon1.svg"
-            },
-            {
-                "id": "2",
-                "name": "RESERVA",
-                "countInPack": 24,
-                "price": 20,
-                "imgUrl": "img/icons/icon2.svg"
-            },
-            {   
-                "id": "3",
-                "name": "LIMITADA",
-                "countInPack": 6,
-                "price": 27,
-                "imgUrl": "img/icons/icon1.svg"
-            }
-        ]
-        `;
-        const products = JSON.parse(productJSON);
-        renderProducts(products);
+        // const productJSON =`[
+        //     {   
+        //         "id": "1",
+        //         "name": "ESTANDAR",
+        //         "countInPack": 10,
+        //         "price": 12, 
+        //         "imgUrl": "img/icons/icon1.svg"
+        //     },
+        //     {
+        //         "id": "2",
+        //         "name": "RESERVA",
+        //         "countInPack": 24,
+        //         "price": 20,
+        //         "imgUrl": "img/icons/icon2.svg"
+        //     },
+        //     {   
+        //         "id": "3",
+        //         "name": "LIMITADA",
+        //         "countInPack": 6,
+        //         "price": 27,
+        //         "imgUrl": "img/icons/icon1.svg"
+        //     }
+        // ]
+        // `;
+        // const products = JSON.parse(productJSON);
+        // renderProducts(products);
 
-        // async function loadProducts (){
-        //         const response = await fetch('products.json');
-        //         const products = await response.json();
-        //         renderProducts(products);
-        // };
-        // loadProducts();
+        async function loadProducts (){
+                const response = await fetch('products.json');
+                const products = await response.json();
+                renderProducts(products);
+        };
+        loadProducts();
 })();
